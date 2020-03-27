26ab7fa726e7d65bac62a4cc9bc3b573a4a22e43
Andrew Maule <developer@bliptrip.net>
2020-03-27 10:08:19 -0500

    Adding frontend/ folder for React-based frontend to GinaDeux.  Amended setup.sh to deal with 'virgin' installation.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,8 +2,18 @@
 #
 # Script to setup the required packages for GinaDeux.  Right now this assumes
 # a Linux OS with apt-type package installation.
+sudo apt --yes update
+sudo apt --yes upgrade
 sudo apt --yes install libopencv-dev nodejs npm python3 python3-pip
 
+if [[ -n $SETUP_VIRGIN ]]; then #This is invoked when creating project from scratch -- if checking out from github, it's unecessary.
+    sudo npm install npx -g
+    sudo npm install create-react-app -g
+    npx create-react-app frontend
+    cd frontend
+    npm install --save google-map-react
+    cd ..
+fi
 cd frontend
 npm install #Install dependencies found in package.json
 cd ../
