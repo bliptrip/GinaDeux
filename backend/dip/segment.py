18dd10ff89b3cbadc07d36087ef5ec6ff59624d4
Andrew Maule <maule2@wisc.edu>
2020-04-14 10:46:10 -0500

    Fixing syntactical errors.

--- backend/dip/segment.py
+++ backend/dip/segment.py
@@ -21,23 +21,25 @@ class Segment():
         return
 
     def preprocess(self, image):
-        image = cv2.resize(image, 0, self.resize, self.resize) 
-        avg = mean(image, disk(4))
+        image = cv2.resize(image, (0,0), fx=self.resize, fy=self.resize) 
+        avg = np.zeros(image.shape, dtype='uint8')
+        for c in range(image.shape[2]):
+            avg[:,:,c] = mean(image[:,:,c], disk(4))
         return(avg)
 
     def postprocess(self, binimage):
-        numOnes = np.where(binimage).size
+        numOnes = np.where(binimage)[0].size
         numZeros = binimage.size - numOnes
         if( numOnes > numZeros ):
-            binimage = not binimage #Invert -- Why, I'm not sure, but this was in original GiNA code
-        binimage = closing(binimage, disk(3,3)) #Luis did this image postprocessing to remove noise
+            binimage = ~binimage #Invert -- Why, I'm not sure, but this was in original GiNA code -- maybe had to do with putting green berries on black background
+        binimage = closing(binimage, disk(3)) #Luis did this image postprocessing to remove noise
         #bwareaopen doesn't have a direct implementation.  Could just fill in 'holes', and then remove anything that
         #is below a given size in the segmention stage
-        holes = binimage.copy()
-        flood_fill(holes, (0,0), True, inplace=True)
+        holes = binimage.copy().astype('uint8')*255
+        holes = flood_fill(holes, (0,0), 255)
         # invert holes mask, img fill in holes
-        holes = not holes
-        binimage = binimage | holes
+        holes = ~holes
+        binimage = binimage | holes.astype('bool')
         return(clear_border(binimage))
 
     def segment(self, binimage):
@@ -45,5 +47,7 @@ class Segment():
         Returns a set of region properties on labeled image using skimage
         '''
         label_image = label(binimage)
-        regions = filter(lambda prop: (prop.area >= minArea) and (prop.area <= maxArea), regionprops(label_image))
+        #Extract regions/blobs
+        regions = filter(lambda prop: (prop.area >= self.minArea) and (prop.area <= self.maxArea), regionprops(label_image))
+        #Extract contours/boundaries of each blob
         return(regions)
