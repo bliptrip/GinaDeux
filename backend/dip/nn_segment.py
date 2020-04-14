996381036ae23db8490defec916b10035156a6e5
Andrew Maule <maule2@wisc.edu>
2020-04-14 10:38:42 -0500

    Basic testing of neural network segmentation working.

--- backend/dip/nn_segment.py
+++ backend/dip/nn_segment.py
@@ -6,20 +6,21 @@
 from keras import models
 from keras import layers
 from keras.callbacks import EarlyStopping
+import numpy as np
 from segment import Segment
 
 
 class NNSegment(Segment):
-    def __init__(self, modelPath=None):
+    def __init__(self, modelPath=None, **kwargs):
         '''
         Neural network segmentation class.  If a pretrained model is passed to a constructor, then it is load
         '''
-        super().__init__()
+        super().__init__(**kwargs)
         if modelPath:
             json_file = open('{}.json'.format(modelPath), 'r')
             loaded_model_json = json_file.read()
             json_file.close()
-            self.model = model_from_json(loaded_model_json)
+            self.model = models.model_from_json(loaded_model_json)
             # load weights into new model
             self.model.load_weights("{}.h5".format(modelPath))
         else:
@@ -33,7 +34,7 @@ class NNSegment(Segment):
         the foreground pixel values and one or more images representing the background 
         pixel values.
         '''
-        self.model = models.Sequential()
+        model = self.model = models.Sequential()
         #Recast foreground image into a 3*num_pixels (channels) vector with label value = 1 for foreground
         #Recast background image into a 3*num_pixels (channels) vector with label value = 0 for background
         row,col,nchannel = foreground.shape
@@ -45,7 +46,7 @@ class NNSegment(Segment):
         np.random.seed(seed) #Set seed
         combined_samples = np.random.permutation(np.concatenate((foreground,background)))
         np.random.seed(seed) #Reset seed -- Needed to have category labels consistent with permuted input samples
-        combined_sample_categories  = np.concatenate((foreground_categories,background_categories))
+        combined_sample_categories  = np.random.permutation(np.concatenate((foreground_categories,background_categories)))
         self.model.add(layers.Dense(hidden_unit_size, activation='relu', input_shape=(3,)))
         self.model.add(layers.Dense(1, activation='sigmoid'))
         self.model.compile(optimizer='rmsprop',
@@ -58,12 +59,12 @@ class NNSegment(Segment):
         earlyStoppingC  = EarlyStopping(patience=3, verbose=1, restore_best_weights=True)
         #Recast background image into a 3*num_pixels (channels) vector with label value = 0 for background
         #Randomly mix and concatentate the foreground/background values and their corresponding labels
-        model.add(layers.Dense(hidden_unit_size, activation='relu', input_shape=(3,)))
-        model.add(layers.Dense(16, activation='relu'))
-        model.add(layers.Dense(1, activation='sigmoid'))
-        model.compile()
-        history = self.model.fit(   x=np.transpose(combined_samples), 
-                                    y=np.transpose(combined_sample_categories), 
+        #model.add(layers.Dense(hidden_unit_size, activation='relu', input_shape=(3,)))
+        #model.add(layers.Dense(16, activation='relu'))
+        #model.add(layers.Dense(1, activation='sigmoid'))
+        #model.compile()
+        history = self.model.fit(   x=combined_samples, 
+                                    y=combined_sample_categories[:,np.newaxis], 
                                     epochs=20, 
                                     verbose=1, 
                                     callbacks=[earlyStoppingC], 
@@ -76,16 +77,16 @@ class NNSegment(Segment):
     def predict(self, image):
         image = super().preprocess(image) #Handle global segment preprocessing on image before doing binary classification
         row,col,nchannel = image.shape
-        image.reshape((row*col,nchannel));
+        image = np.reshape(image, newshape=(row*col,nchannel));
         predictions = (self.model.predict(image) >= 0.5) #Cast to a boolean -- anything more than 50% is a foreground pixel
-        predictions.reshape(row,col)
+        predictions = np.reshape(predictions,newshape=(row,col))
         predictions = super().postprocess(predictions)
         return(predictions)
 
     def export(self, path):
         model_json = self.model.to_json()
         with open("{}.json".format(path), "w") as json_file:
-                json_file.write(model_json)
-                json_file.close()
-                # serialize weights to HDF5
-                self.model.save_weights("{}.h5".format(path))
+            json_file.write(model_json)
+            json_file.close()
+            # serialize weights to HDF5
+            self.model.save_weights("{}.h5".format(path))
