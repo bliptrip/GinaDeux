8f0cec799b042e40fad5e259d86225054586d11b
Maule <maule2@wisc.edu>
2020-04-21 13:10:07 -0500

    Adding helper script to perform differences b/w Gina and GinaDeux output for comparison.

---
+++ backend/dip/segment_diff.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+#
+# Author: Andrew Maule
+# Objective: Test script for segmentation funcitonality.
+
+import argparse
+import numpy as np
+import pandas as pd
+import sys
+
+#Local libs
+
+#
+# construct the argument parser and parse the arguments
+def parse_args():
+    parser = argparse.ArgumentParser(prog=sys.argv[0], description="Compute the difference b/w Matlab GiNA output and that of GiNADeux")
+    parser.add_argument('--i1', '--input1', dest='input1', required=True, help="Path to Matlab GiNA output table.")
+    parser.add_argument('--i2', '--input2', dest='input2', required=True, help="Path to GiNADeux output table.")
+    parser.add_argument('-o', '--output', dest='output', required=True, help="Path to comparison table b/w two GiNA versions (CSV format)")
+    parsed = parser.parse_args(sys.argv[1:])
+    return(parsed)
+
+if __name__ == '__main__':
+    parsed = parse_args();
+    ginaUnTable     = pd.read_csv(parsed.input1)
+    ginaDeuxTable   = pd.read_csv(parsed.input2)
+    commonFields    = np.intersect1d(ginaUnTable.columns,ginaDeuxTable.columns)
+    diffTable       = ginaUnTable[commonFields] - ginaDeuxTable[commonFields]
+    diffTable.to_csv(parsed.output)
+
